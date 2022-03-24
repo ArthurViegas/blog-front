b453df2ee1d2c1306bd5f0ae2bcaca25a1a570b1
Arthur <viegas.arthur2013@hotmail.com>
2022-03-24 12:25:22 -0300

    Set provider to load all posts into state

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppContext from './AppContext';
+import { requestApi } from '../services/blogApi'
 
 function AppProvider({ children }) {
+  const [allPosts, setPosts] = useState([])
+  const getAllPosts = async () => {
+    const posts = await requestApi();
+    setPosts(posts)
+  }
+  useEffect(() => {
+    getAllPosts();
+  }, []);
+
   return(
-    <AppContext.Provider value={{}}>
+    <AppContext.Provider value={{allPosts}}>
       { children }
       </AppContext.Provider>
   );
