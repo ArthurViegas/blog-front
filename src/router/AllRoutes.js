94a3423a5dbd9eb8f37774fbc84339f2c2eb0355
Arthur <viegas.arthur2013@hotmail.com>
2022-03-24 20:03:23 -0300

    Add login and register routes

--- src/router/AllRoutes.js
+++ src/router/AllRoutes.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import Main from '../pages/Main';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 function AllRoutes() {
   return(
     <Routes>
       <Route exact path="/" element={ <Main /> } />
+      <Route exact path="/login" element={ <Login /> } />
+      <Route exact path="/register" element={ <Register /> } />
     </Routes>
   )
 }
